a6207ca9d3d55c3f83667a9993abe7d575913076
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-05-17 19:39:46 -0300

    Update BDEX1.sql

--- Exercicios SQL/EXERC_FIXACAO/BDEX1.sql
+++ Exercicios SQL/EXERC_FIXACAO/BDEX1.sql
@@ -1,1 +1,17 @@
+#Usando a base de dados BDEX1_HOSPITAL, responda:
 
+# 1 - Exibir todas as informações da tabela PACIENTES.
+# 2 - Mostrar os nomes e e-mails dos pacientes que nasceram depois do ano 2000.
+# 3 - Listar os nomes dos médicos e as especialidades em que trabalham.
+# 4 - Exibir os detalhes das consultas: ID da consulta, data, hora, nome do médico e nome do paciente.
+# 5 - Listar o nome dos pacientes e quantas consultas cada um fez.
+# 6 - Mostrar as especialidades de médicos que fizeram consultas em 2023.
+# 7 - Listar todos os departamentos, ordenados pelo nome da especialidade de A a Z.
+# 8 - Mostrar os nomes e e-mails dos médicos que não têm telefone cadastrado.
+# 9 - Exibir os nomes dos pacientes que têm consultas com médicos do departamento de Cardiologia.
+# 10 - Para cada especialidade, mostrar o nome da especialidade e a média de consultas realizadas pelos médicos dessa especialidade.
+# 11 - Contar quantos pacientes têm um telefone cadastrado.
+# 12 - Listar os nomes dos médicos que fizeram mais de 10 consultas.
+# 13 - Mostrar o nome dos pacientes e a data da última consulta que eles fizeram.
+# 14 - Contar quantas consultas cada médico realizou.
+# 15 - Para cada paciente que fez uma consulta, mostrar o CPF, o nome e a porcentagem de consultas em relação ao total de consultas feitas por todos os pacientes.
