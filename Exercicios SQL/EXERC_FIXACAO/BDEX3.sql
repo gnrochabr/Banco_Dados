3aa6821ccdd105f880b45d0700d8712cd28abba7
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-05-24 07:33:17 -0300

    Create BDEX3.sql

---
+++ Exercicios SQL/EXERC_FIXACAO/BDEX3.sql
@@ -0,0 +1,17 @@
+
+USE BDEX3_REDESOCIAL;
+#1. Exibir todas as informações da tabela `USUARIOS`.
+#2. Mostrar os nomes, sobrenomes e e-mails de todos os usuários.
+#3. Listar os nomes e descrições de todos os grupos.
+#4. Exibir os detalhes das postagens: ID da postagem, texto, data e hora, e o nome do usuário que postou.
+#5. Listar o nome dos usuários e a quantidade de postagens que cada um fez.
+#6. Mostrar os nomes dos usuários que fizeram comentários em 2024.
+#7. Listar todos os grupos, ordenados pelo nome do grupo de A a Z.
+#8. Mostrar os nomes dos grupos que têm a palavra "Tecnologia" na descrição.
+#9. Exibir os detalhes dos comentários: ID do comentário, texto, data e hora, nome do usuário que comentou e ID da postagem comentada.
+#10. Para cada postagem, mostrar o ID da postagem e a quantidade de comentários.
+#11. Contar quantos seguidores cada usuário possui.
+#12. Listar os nomes dos usuários que pertencem ao grupo "Desenvolvimento".
+#13. Mostrar o nome dos usuários e a data e hora da última postagem que eles fizeram.
+#14. Contar quantas postagens cada usuário fez.
+#15. Para cada grupo, exibir o nome do grupo e a quantidade de usuários que pertencem a ele.
