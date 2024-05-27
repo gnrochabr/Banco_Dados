cc51e55f7df00483170865c0f1eb8610f021a613
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-05-27 14:23:16 -0300

    Update BDEX2.sql

--- Exercicios SQL/EXERC_FIXACAO/BDEX2.sql
+++ Exercicios SQL/EXERC_FIXACAO/BDEX2.sql
@@ -1,11 +1,13 @@
 #Usando a base de dados BDEX2_PASSAGENS_AEREAS, responda:
 
+USE BDEX2_PASSAGENS_AEREAS;
+
 # 1 - Exibir todas as informações da tabela PASSAGEIROS.
 # 2 - Mostrar os nomes e sobrenomes dos passageiros que nasceram depois do ano 1990.
 # 3 - Listar os nomes dos aeroportos e as cidades onde estão localizados.
 # 4 - Exibir os detalhes das reservas: ID da reserva, data da reserva, forma de pagamento e valor da reserva.
 # 5 - Listar o nome das companhias aéreas e quantas aeronaves cada uma possui.
-# 6 - Mostrar os nomes dos passageiros que fizeram reservas em 2023.
+# 6 - Mostrar os nomes dos passageiros que fizeram reservas em 2024.
 # 7 - Listar todas as companhias aéreas, ordenadas pelo nome fantasia de A a Z.
 # 8 - Mostrar os nomes dos aeroportos que têm pista com tamanho maior que 3000 metros.
 # 9 - Exibir os detalhes dos voos: ID do voo, assento, aeroporto de partida, data e hora de partida, aeroporto de chegada, data e hora de chegada, modelo da aeronave e nome da companhia aérea.
@@ -14,4 +16,4 @@
 # 12 - Listar os nomes das aeronaves e o ano de fabricação que possuem mais de 150 poltronas.
 # 13 - Mostrar o nome dos passageiros e a data da última reserva que eles fizeram.
 # 14 - Contar quantas reservas foram feitas por cada passageiro.
-# 15 - Para cada reserva feita em 2023, mostrar o ID da reserva, o valor da reserva e a porcentagem desse valor em relação ao valor total de reservas feitas em 2023.
+# 15 - Para cada reserva feita em 2024, mostrar o ID da reserva, o valor da reserva e a porcentagem desse valor em relação ao valor total de reservas feitas em 2024.
