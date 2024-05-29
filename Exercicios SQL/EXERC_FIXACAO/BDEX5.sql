9a74a9ba22013ace592515d3d173d7b2be2d6cac
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-05-29 09:49:53 -0300

    Create BDEX5.sql

---
+++ Exercicios SQL/EXERC_FIXACAO/BDEX5.sql
@@ -0,0 +1,16 @@
+use BDEX5_EVENTOS;
+#1. Exibir todas as informações da tabela PARTICIPANTES.
+#2. Mostrar os nomes e empresas dos participantes que nasceram depois do ano 1990.
+#3. Listar os nomes dos eventos e as entidades organizadoras.
+#4. Exibir os detalhes das atividades: ID da atividade, nome da atividade, data, hora, nome do evento.
+#5. Listar o nome dos participantes e quantas atividades cada um participou.
+#6. Mostrar os nomes dos eventos realizados em 2023.
+#7. Listar todos os locais, ordenados pelo nome da cidade de A a Z.
+#8. Mostrar os nomes e CPFs dos participantes que não têm empresa cadastrada.
+#9. Exibir os nomes dos participantes que estão inscritos em eventos realizados no local com nome 'Centro de Convenções'.
+#10. Para cada entidade organizadora, mostrar o nome da entidade e a média de eventos realizados.
+#11. Contar quantos participantes têm uma empresa cadastrada.
+#12. Listar os nomes dos participantes que participaram de mais de 5 atividades.
+#13. Mostrar o nome dos participantes e a data da última atividade que eles participaram.
+#14. Contar quantas atividades cada evento possui.
+#15. Para cada participante que participou de um evento, mostrar o CPF, o nome e a porcentagem de eventos em relação ao total de eventos realizados.
