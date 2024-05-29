067fc50c611df4a931f9593263270a3a06f05f0d
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-05-29 09:53:22 -0300

    Create BDEX7.sql

---
+++ Exercicios SQL/EXERC_FIXACAO/BDEX7.sql
@@ -0,0 +1,16 @@
+use BDEX7_TRANSACOESBANCARIAS;
+#1. Criar uma consulta para exibir todas as informações da tabela USUARIOS.
+#2. Escrever uma consulta para mostrar os nomes e CPFs dos usuários que possuem telefone cadastrado.
+#3. Listar os nomes dos bancos e suas respectivas agências.
+#4. Exibir os detalhes das transações: ID da transação, valor, natureza (crédito ou débito), nome do usuário.
+#5. Listar os nomes dos usuários e seus e-mails, ordenados pelo nome do usuário de forma alfabética.
+#6. Mostrar as informações das contas bancárias que possuem saldo superior a 1000 reais.
+#7. Listar os nomes dos usuários que realizaram transações com valor superior a 500 reais.
+#8. Exibir os detalhes das transações que foram realizadas em contas do banco "Banco Central".
+#9. Mostrar os nomes dos usuários e seus respectivos telefones cadastrados, ordenados pelo nome do usuário de forma decrescente.
+#10. Listar os nomes dos bancos e o total de contas cadastradas em cada um deles.
+#11. Para cada usuário, mostrar o nome do usuário e o saldo atual de sua conta bancária.
+#12. Contar quantas transações de crédito foram realizadas.
+#13. Listar os nomes dos usuários que realizaram transações bancárias no último mês.
+#14. Exibir os detalhes das transações que foram realizadas em contas com saldo negativo.
+#15. Mostrar o valor total de transações de débito realizadas no último dia útil.
