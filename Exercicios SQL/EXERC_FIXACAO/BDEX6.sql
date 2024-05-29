61c8d7925504a1a820b23995ae3bb0f35a6dc2e3
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-05-29 09:51:57 -0300

    Create BDEX6.sql

---
+++ Exercicios SQL/EXERC_FIXACAO/BDEX6.sql
@@ -0,0 +1,16 @@
+use BDEX6_CINEMA;
+#1. Criar uma consulta para exibir todas as informações da tabela ESPECTADORES.
+#2. Escrever uma consulta para mostrar os nomes e e-mails dos espectadores que nasceram antes de 1995.
+#3. Listar os nomes das salas de cinema e suas capacidades.
+#4. Exibir os detalhes das sessões de cinema: ID da sessão, nome do filme, nome da sala.
+#5. Listar o nome e o distribuidor dos filmes com tempo de exibição superior a 120 minutos.
+#6. Mostrar as informações das sessões que exibiram filmes do diretor "Christopher Nolan".
+#7. Listar os nomes e as capacidades das salas de cinema ordenadas pela capacidade de forma decrescente.
+#8. Exibir os detalhes dos ingressos: ID do ingresso, data, hora, nome do filme e nome do espectador.
+#9. Mostrar os nomes e as idades dos espectadores que têm telefone cadastrado.
+#10. Listar os nomes dos filmes que foram exibidos em mais de uma sessão.
+#11. Para cada sala de cinema, mostrar o nome da sala e a quantidade total de ingressos vendidos.
+#12. Contar quantos espectadores têm um e-mail cadastrado.
+#13. Listar os nomes dos filmes que foram exibidos em sessões agendadas para o dia de hoje.
+#14. Exibir os detalhes das sessões que exibiram filmes com tempo de exibição inferior a 90 minutos.
+#15. Mostrar o nome do filme e a data da sessão para todas as sessões agendadas para o próximo sábado.
