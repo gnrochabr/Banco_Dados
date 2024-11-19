93fe4d04db1ec57b4ce085249179e6e443632585
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-11-19 18:31:05 -0300

    Create REVISAO_DQL.sql

---
+++ 2024_2/REVISAO/REVISAO_DQL.sql
@@ -0,0 +1,39 @@
+-- 1. Selecione todos os jogos disponíveis no banco de dados.
+
+-- 2. Liste os nomes e telefones de todos os clientes cadastrados.
+
+-- 3. Encontre todos os jogos lançados após o ano de 2020.
+
+-- 4. Liste todos os clientes que moram na cidade de 'Vila Velha'.
+
+-- 5. Obtenha todas as locações feitas no último mês (considerando a data atual).
+
+-- 6. Liste os jogos disponíveis em ordem crescente de preço da diária.
+
+-- 7. Mostre os 5 clientes mais recentes cadastrados, ordenados pela data de nascimento.
+
+-- 8. Calcule a quantidade de locações feitas por cada cliente.
+
+-- 9. Encontre os jogos mais alugados (com pelo menos 5 locações).
+
+-- 10. Liste os métodos de pagamento e a soma total de valores pagos para cada método.
+
+-- 11. Recupere os detalhes das locações, incluindo o nome do cliente e o título do jogo.
+
+-- 12. Liste os endereços de todos os clientes, com seus respectivos nomes.
+
+-- 13. Mostre todas as locações em que o status é 'Atrasada', incluindo os dados do cliente e o jogo alugado.
+
+-- 14. Calcule o valor médio das diárias dos jogos.
+
+-- 15. Determine o valor total arrecadado em pagamentos.
+
+-- 16. Encontre o jogo mais caro no banco de dados.
+
+-- 17. Liste os 3 jogos mais baratos disponíveis para locação.
+
+-- 18. Mostre as 10 locações mais recentes, incluindo a data de locação e o status.
+
+-- 19. Identifique os clientes que fizeram mais de 3 locações.
+
+-- 20. Liste os jogos com preço médio diário acima de R$5 e que possuem mais de 2 locações.
